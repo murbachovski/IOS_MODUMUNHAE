516eac0e09698a186e2459ad8dc2a433ce68e909
flutter6675 <flutter6675@gmail.com>
2022-07-06 11:57:53 +0900

    Initial Commit

---
+++ AI_MUNJANG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI_MUNJANG
+//
+//  Created by DONG CHEOL KIM on 2022/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
