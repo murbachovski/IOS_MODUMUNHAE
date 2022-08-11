a3dcb65013524897906ab169bfaaa6307d6f955b
seogamjini <seogamjini@gmail.com>
2022-08-11 15:42:04 +0900

    온보딩 이미지를 메인화면에 삽입

---
+++ AI_MUNJANG/BannerCell.swift
@@ -0,0 +1,14 @@
+//
+//  BannerCell.swift
+//  AI_MUNJANG
+//
+//  Created by murba chovski on 2022/08/11.
+//
+
+import UIKit
+
+class BannerCell: UICollectionViewCell {
+    @IBOutlet var title: UILabel!
+    @IBOutlet var bannerImg: UIImageView!
+    
+}
--- AI_MUNJANG/MainViewController.swift
+++ AI_MUNJANG/MainViewController.swift
@@ -21,9 +21,12 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
     var sectionEight :QuizContents = []
 
     var sectionTotal :[QuizContents] = []
+    var dataArray :Array<UIImage> = []
     
-
-    @IBOutlet weak var topTitle: UILabel!
+    var titles :Array<String> = []
+    var nowPage: Int = 0
+    @IBOutlet var bannerCollectionView: UICollectionView!
+    
     @IBOutlet weak var collectionViewEight: UICollectionView!
     
     let munjangElements:[String] = ["주어", "서술어","조사", "어미","관형어","부사어","문장부사어","마침부호"]
@@ -38,76 +41,22 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
         layout.estimatedItemSize = CGSize(width: CGFloat(162).relativeToIphone8Width() , height: CGFloat(80).relativeToIphone8Width())
         collectionViewEight.delegate = self
         collectionViewEight.dataSource = self
+        
+        bannerCollectionView.delegate = self
+        bannerCollectionView.dataSource = self
+        
         collectionViewEight.backgroundColor = .clear
         
         self.navigationItem.backButtonTitle = " "
         setupUI()
         
-      
-        
-//        guard let quizData = readLocalFile(forName: "quizContents") else { print("quizData is null"); return}
-//        guard let quizContents = try? JSONDecoder().decode(QuizContents.self, from: quizData) else { print("quizContens is null");  return }
-//
-//        for  content in quizContents {
-//
-//            if content.section == "1경" {
-//                sectionOne.append(content)
-//            }
-//            if content.section == "2경" {
-//                sectionTwo.append(content)
-//            }
-//            if content.section == "3경" {
-//                sectionThree.append(content)
-//            }
-//            if content.section == "4경" {
-//                sectionFour.append(content)
-//            }
-//            if content.section == "5경" {
-//                sectionFive.append(content)
-//            }
-//            if content.section == "6경" {
-//                sectionSix.append(content)
-//            }
-//            if content.section == "7경" {
-//                sectionSeven.append(content)
-//            }
-//            if content.section == "8경" {
-//                sectionEight.append(content)
-//            }
-//
-//            sectionTotal = [sectionOne, sectionTwo, sectionThree, sectionFour, sectionFive, sectionSix, sectionSeven, sectionEight]
-//
-//        }
-//
-//        print( "sectionOne.count : \(sectionOne.count)")
-//        print("sectionTwo.count: \(sectionTwo.count)")
-//        print("sectionThree.count: \(sectionThree.count)")
-//        print("sectionFour.count: \(sectionFour.count)")
-//
-//        print("sectionFive.count: \(sectionFive.count)")
-//        print("sectionSix.count: \(sectionSix.count)")
-//        print("sectionSeven.count: \(sectionSeven.count)")
-//        print("sectionEight.count: \(sectionEight.count)")
-
-        
     }
     
-//    private func readLocalFile(forName name: String) -> Data? {
-//        do {
-//            if let bundlePath = Bundle.main.path(forResource: name,
-//                                                 ofType: "json"),
-//                let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
-//                return jsonData
-//            }
-//        } catch {
-//            print(error)
-//        }
-//
-//        return nil
-//    }
-    
     func setupUI(){
-        topTitle.font = UIFont(name: "NanumSquareEB", size: 24)
+        nowPage = 0
+        dataArray = [UIImage(named: "illust1.png")!, UIImage(named: "illust2.png")!, UIImage(named: "illust3.png")!]
+        titles = ["문해력의 출발은 \n문장력입니다!","문장공부 \n해야 합니다!", "꾸준하게 키워가는 \n나의 문해력"]
+//        topTitle.font = UIFont(name: "NanumSquareEB", size: 24)
         
         
 
@@ -117,6 +66,8 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
         super.viewWillAppear(animated)
         navigationController?.navigationBar.barTintColor = hexStringToUIColor(hex: "#F9F9F9")
         print("사용자가 구독 중인가? : \(Core.shared.isUserSubscription())")
+        
+        bannerTimer()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -137,36 +88,54 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20)
+        
+        if collectionView == bannerCollectionView {
+                return UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20)
+        }   else {
+                return UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20)
+        }
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return munjangElements.count
+        if collectionView == bannerCollectionView {
+                return 3
+        }   else {
+                return munjangElements.count
+        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? MunjangEightCollectionViewCell else {
-               return UICollectionViewCell()
-           }
+        if collectionView == bannerCollectionView {
+            let cell = bannerCollectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as! BannerCell
+                    cell.bannerImg.image = dataArray[indexPath.row]
+            cell.title.text = titles[indexPath.row]
+                    return cell
+        }   else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? MunjangEightCollectionViewCell else {
+                   return UICollectionViewCell()
+               }
+            
+            //셀의 내용 채우기
+            cell.digitTitle.text = "\(indexPath.row + 1)경"
+            
+            cell.mainTitle.text = munjangElements[indexPath.row]
+            cell.mainTitle.font = UIFont(name: "NanumSquareEB", size: 15)
+            cell.subTitle.text = subElements[indexPath.row]
+            
+            //셀에 shadow추가
+            cell.backgroundColor = .white
+            cell.layer.cornerRadius = 10
+            cell.layer.shadowOpacity = 0.8
+            cell.layer.shadowColor = UIColor.lightGray.cgColor
+            cell.layer.shadowOffset = CGSize(width: 1, height: 1)
+            cell.layer.shadowRadius = 2
+            cell.layer.masksToBounds = false
+            
+            
+                    
+            return cell
+        }
         
-        //셀의 내용 채우기
-        cell.digitTitle.text = "\(indexPath.row + 1)경"
-        
-        cell.mainTitle.text = munjangElements[indexPath.row]
-        cell.mainTitle.font = UIFont(name: "NanumSquareEB", size: 15)
-        cell.subTitle.text = subElements[indexPath.row]
-        
-        //셀에 shadow추가
-        cell.backgroundColor = .white
-        cell.layer.cornerRadius = 10
-        cell.layer.shadowOpacity = 0.8
-        cell.layer.shadowColor = UIColor.lightGray.cgColor
-        cell.layer.shadowOffset = CGSize(width: 1, height: 1)
-        cell.layer.shadowRadius = 2
-        cell.layer.masksToBounds = false
-        
-        
-                
-        return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -184,5 +153,40 @@ class MainViewController: UIViewController, UICollectionViewDataSource, UICollec
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
        return 14 // Keep whatever fits for you
      }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+//        if scrollView == bannerCollectionView
+            nowPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
+        }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        if collectionView == bannerCollectionView {
+//            return CGSize(width: bannerCollectionView.frame.size.width  , height:  bannerCollectionView.frame.height)
+//        }
+//        return CGSize.zero
+//    }
+    
+    // 2초마다 실행되는 타이머
+        func bannerTimer() {
+            let _: Timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (Timer) in
+                self.bannerMove()
+            }
+        }
+        // 배너 움직이는 매서드
+        func bannerMove() {
+            print("nowPage: \(nowPage)")
+            // 현재페이지가 마지막 페이지일 경우
+            if nowPage == dataArray.count-1 {
+            // 맨 처음 페이지로 돌아감
+                bannerCollectionView.scrollToItem(at: NSIndexPath(item: 0, section: 0) as IndexPath, at: .right, animated: true)
+                nowPage = 0
+                return
+            }
+            // 다음 페이지로 전환
+            nowPage += 1
+            bannerCollectionView.scrollToItem(at: NSIndexPath(item: nowPage, section: 0) as IndexPath, at: .right, animated: true)
+        }
 }
+
+
 
